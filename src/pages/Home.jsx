c6746876f4a5e8e035683d86b51cddf9db385541
Kirman442 <kirill@irminlabs.com>
2025-01-30 13:46:47 +0100

    clean page from unused code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,8 @@ import React, { useEffect, useState } from "react";
 import Head from '../components/Head';
 import ContactForm from "../components/ContactForm";
 
-import { Link } from "react-router-dom";
-// import HomeGallery from "../components/HomeGallery";
-
-import { LazyLoadComponent } from 'react-lazy-load-image-component';
+// import { Link } from "react-router-dom";
+// import { LazyLoadComponent } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const isWebpSupported = () => {
@@ -65,9 +63,6 @@ const Home = () => {
                 <div className="container">
                     <div className="row align-items-center">
                         <div className="col-lg-5 col-md-6 text-center md-margin-30px-bottomt wow animate__fadeInLeft">
-                            {/* <img src="images/about-img3.jpg" alt="" className="border-radius-6 w-100" data-no-retina=""> */}
-                            {/* <img className="border-radius-6 w-100" src='images/main/about-img3.jpg'
-                                alt="Foto" data-no-retina /> */}
                             <img className="border-radius-6 w-100" src={`${import.meta.env.BASE_URL}images/main/about-img3.${webpSupported ? 'webp' : 'jpg'}`}
                                 alt="Foto" data-no-retina />
                         </div>
@@ -78,8 +73,6 @@ const Home = () => {
                                 Наши специалисты помогают грамотно урегулировать финансовые трудности, минимизируя риски и потери.
                                 Опираясь на многолетний опыт, мы разрабатываем стратегии, которые ведут к оптимальному решению сложных ситуаций.
                                 Доверьтесь профессионалам – мы берём на себя все юридические и организационные вопросы, чтобы Вы могли сосредоточиться на новом этапе жизни.</p>
-                            {/* <Link className="btn btn-dark-gray btn-small text-extra-small btn-rounded margin-5px-top" to="Services" aria-label="Services link"><i className="margin-5px-right no-margin-left" aria-hidden="true"></i>Наши услуги</Link> */}
-                            {/* <a href="services" className="btn btn-dark-gray btn-small text-extra-small btn-rounded margin-5px-top"><i className="fa-solid fa-circle-play icon-very-small margin-5px-right no-margin-left" aria-hidden="true"></i> Наши услуги</a> */}
                         </div>
                     </div>
                     <div className="divider-full margin-seven-bottom margin-three-top"></div>
@@ -99,108 +92,11 @@ const Home = () => {
                             </p>
                         </div>
                     </div>
-                    {/* <div className="row row-cols-1 row-cols-lg-3 row-cols-sm-2 justify-content-center">
-                        <div className="col md-margin-50px-bottom sm-margin-40px-bottom last-paragraph-no-margin">
-                            <div className="feature-box-5 position-relative">
-                                <i className="fas fa-desktop text-medium-gray icon-medium"></i>
-                                <div className="feature-content">
-                                    <div className="text-extra-dark-gray margin-10px-bottom alt-font font-weight-600">Digital Solutions</div>
-                                    <p className="d-inline-block w-75 lg-w-100 xs-w-90">Lorem Ipsum is simply text the printing and typesetting standard industry.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col md-margin-50px-bottom sm-margin-40px-bottom last-paragraph-no-margin">
-                            <div className="feature-box-5 position-relative">
-                                <i className="icon-book-open text-medium-gray icon-medium"></i>
-                                <div className="feature-content">
-                                    <div className="text-extra-dark-gray margin-10px-bottom alt-font font-weight-600">SEO Marketing</div>
-                                    <p className="d-inline-block w-75 lg-w-100 xs-w-90">Lorem Ipsum is simply text the printing and typesetting standard industry.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col last-paragraph-no-margin">
-                            <div className="feature-box-5 position-relative">
-                                <i className="icon-gift text-medium-gray icon-medium"></i>
-                                <div className="feature-content">
-                                    <div className="text-extra-dark-gray margin-10px-bottom alt-font font-weight-600">Creative Strategy</div>
-                                    <p className="d-inline-block w-75 lg-w-100 xs-w-90">Lorem Ipsum is simply text the printing and typesetting standard industry.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div> */}
                 </div>
             </section>
 
-            {/* <section className="lg-padding-two-lr md-no-padding-lr" style={{ backgroundImage: `url(${backgroungImage})`, visibility: "visible" }}>
-                <div className="container">
-                    <div className="row justify-content-center">
-                        <div className="col-lg-8 col-sm-10 margin-six-bottom text-center last-paragraph-no-margin">
-                            <div className="alt-font text-deep-pink margin-10px-bottom text-uppercase text-small">Больше причин доверить нам свои проблемы</div>
-                            <h5 className="alt-font text-extra-dark-gray font-weight-400 mb-0">Сложные виды банкротств и поиск активов</h5>
-                        </div>
-                    </div>
-                    <div className="row row-cols-1 row-cols-lg-3 row-cols-sm-2">
-                        <div className="col margin-six-bottom md-margin-50px-bottom sm-margin-40px-bottom wow animate__fadeInUp last-paragraph-no-margin">
-                            <div className="feature-box-5 position-relative">
-                                <div className="feature-content">
-                                    <div className="text-extra-dark-gray margin-10px-bottom alt-font font-weight-600">Банкротство физ лиц</div>
-                                    <p className="d-inline-block sm-w-90">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col margin-six-bottom md-margin-50px-bottom sm-margin-40px-bottom wow animate__fadeInUp last-paragraph-no-margin">
-                            <div className="feature-box-5 position-relative">
-                                <div className="feature-content">
-                                    <div className="text-extra-dark-gray margin-10px-bottom alt-font font-weight-600">Банкротство юр лиц</div>
-                                    <p className="d-inline-block sm-w-90">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col margin-six-bottom md-margin-50px-bottom sm-margin-40px-bottom wow animate__fadeInUp last-paragraph-no-margin">
-                            <div className="feature-box-5 position-relative">
-                                <div className="feature-content">
-                                    <div className="text-extra-dark-gray margin-10px-bottom alt-font font-weight-600">Поиск активов</div>
-                                    <p className="d-inline-block sm-w-90">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col md-margin-50px-bottom sm-margin-40px-bottom wow animate__fadeInUp last-paragraph-no-margin">
-                            <div className="feature-box-5 position-relative ">
-                                <div className="feature-content">
-                                    <div className="text-extra-dark-gray margin-10px-bottom alt-font font-weight-600">Арбитраж</div>
-                                    <p className="d-inline-block sm-w-90">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col xs-margin-40px-bottom wow animate__fadeInUp last-paragraph-no-margin">
-                            <div className="feature-box-5 position-relative">
-                                <div className="feature-content">
-                                    <div className="text-extra-dark-gray margin-10px-bottom alt-font font-weight-600">Кассация</div>
-                                    <p className="d-inline-block sm-w-90">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col wow animate__fadeInUp last-paragraph-no-margin">
-                            <div className="feature-box-5 position-relative">
-                                <div className="feature-content">
-                                    <div className="text-extra-dark-gray margin-10px-bottom alt-font font-weight-600">Реструктуризация долгов</div>
-                                    <p className="d-inline-block sm-w-90">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section> */}
-
             <section className="wow animate__fadeIn bg-light-gray" >
                 <div className="container">
-                    {/* <div className="row justify-content-center">
-                        <div className="col-lg-7 text-center margin-100px-bottom sm-margin-40px-bottom">
-                            <div className="position-relative overflow-hidden w-100">
-                                <span className="text-small text-outside-line-full alt-font font-weight-600 text-uppercase">Tab Style 02</span>
-                            </div>
-                        </div>
-                    </div> */}
                     <div className="row justify-content-center">
                         <div className="col-lg-8 col-sm-10 margin-six-bottom text-center last-paragraph-no-margin">
                             <div className="alt-font text-green margin-10px-bottom text-uppercase text-small">Больше причин доверить нам свои проблемы</div>
@@ -212,17 +108,14 @@ const Home = () => {
                         <div className="col-lg-8 text-center tab-style3" id="animated-tab1">
                             {/* <!-- start tab navigation --> */}
                             <ul className="nav nav-tabs margin-50px-bottom sm-margin-30px-bottom text-uppercase alt-font text-small font-weight-600 justify-content-center flex-column flex-md-row" role="tablist">
-                                <li className="nav-item" role="presentation"><a href="#tab3_sec1" data-bs-toggle="tab" className="nav-link text-medium-gray active" aria-selected="true" role="tab" >Арбитраж</a></li>
-                                <li className="nav-item" role="presentation"><a href="#tab3_sec2" data-bs-toggle="tab" className="nav-link text-medium-gray " aria-selected="false" tabindex="-1" role="tab" >Банкротство</a></li>
-                                {/* <li className="nav-item" role="presentation"><a href="#tab3_sec3" data-bs-toggle="tab" className="nav-link text-medium-gray" aria-selected="false" role="tab" tabindex="-1">Production</a></li>
-                                <li className="nav-item" role="presentation"><a href="#tab3_sec4" data-bs-toggle="tab" className="nav-link text-medium-gray" aria-selected="false" tabindex="-1" role="tab">Happy Client</a></li> */}
+                                <li className="nav-item" role="presentation"><a href="#tab3_sec1" data-bs-toggle="tab" className="text-medium nav-link text-medium-gray active" aria-selected="true" role="tab" >Арбитраж</a></li>
+                                <li className="nav-item" role="presentation"><a href="#tab3_sec2" data-bs-toggle="tab" className="text-medium nav-link text-medium-gray " aria-selected="false" tabindex="-1" role="tab" >Банкротство</a></li>
                             </ul>
                             {/* <!-- end tab navigation --> */}
                         </div>
                     </div>
 
                     <div className="tab-content">
-
                         <div id="tab3_sec1" className="tab-pane fade in active show" role="tabpanel">
                             <div className="row justify-content-center">
                                 <div className="row row-cols-1 row-cols-lg-3 row-cols-md-2 justify-content-center">
@@ -383,29 +276,7 @@ const Home = () => {
                                 </div>
                             </div>
                         </div>
-
-
-                        {/* <div id="tab3_sec3" className="tab-pane fade in" role="tabpanel">
-                            <div className="row justify-content-center">
-                                <div className="col-lg-8 last-paragraph-no-margin">
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and industry. Lorem Ipsum typesetting has dummy text.</p>
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div id="tab3_sec4" className="tab-pane fade in" role="tabpanel">
-                            <div className="row justify-content-center">
-                                <div className="col-lg-8 last-paragraph-no-margin">
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and industry. Lorem Ipsum typesetting has dummy text.</p>
-                                </div>
-                            </div>
-                        </div> */}
-
                     </div>
-
                 </div>
             </section>
 
@@ -458,9 +329,6 @@ const Home = () => {
 
                 <ContactForm />
             </section >
-
-
-
         </div >
     );
 };
